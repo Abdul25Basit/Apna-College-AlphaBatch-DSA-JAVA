d422879a0c6536ab9c8c85eac53d5b1ec987b8c1
Abdul25Basit <136110055+Abdul25Basit@users.noreply.github.com>
2024-08-06 22:18:49 +0530

    Update first_occurence_of_key.java

--- Recursion/first_occurence_of_key.java
+++ Recursion/first_occurence_of_key.java
@@ -1,21 +1,28 @@
-public class first_occurence_of_key{
-    public static void main(String args[]){
+public class FirstOccurrence {
 
-        int arr[] = {8,3,6,9,5,10,2,5,3};
-        int key = 5;
-        System.out.print(Position(arr,key,0));
+    /**
+     * Finds the index of the first occurrence of a given key in an integer array.
+     *
+     * @param arr The input integer array.
+     * @param key The key to search for.
+     * @return The index of the first occurrence of the key, or -1 if not found.
+     */
+    public static int firstOccurrence(int[] arr, int key) {
+        // Iterate through the array
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                // Return the index if the key is found
+                return i;
+            }
+        }
+        // Key not found
+        return -1;
     }
 
-    public static int Position(int arr[],int key,int i){
-
-        if(i == arr.length-1){
-            return -1;
-        }
-        
-        if(arr[i] == key){
-            return i;
-        }
-
-        return Position(arr,key,i+1);
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 6, 9, 5, 10, 2, 5, 3};
+        int key = 5;
+        int index = firstOccurrence(arr, key);
+        System.out.println(index); // Output: 4
     }
 }
