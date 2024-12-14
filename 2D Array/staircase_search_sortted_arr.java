35f488c6bf338238f6146e53561e54bc599210b5
Abdul25Basit <136110055+Abdul25Basit@users.noreply.github.com>
2024-12-14 21:17:40 +0530

    staircase_search_sortted_arr.java

--- 2D Array/staircase_search_sortted_arr.java
+++ 2D Array/staircase_search_sortted_arr.java
@@ -1,58 +1,55 @@
 public class staircase_search_sortted_arr {
-    // we are starting search from "Right Upper Corner"
-    public static boolean staircase_Search_upperRight(int matrix[][], int key) {
 
+    // Staircase search starting from the upper-right corner
+    public static boolean staircase_Search_upperRight(int matrix[][], int key) {
+        // Start at the top-right corner
         int row = 0, col = matrix[0].length - 1;
 
+        // Loop until we either find the key or move out of bounds
         while (row < matrix.length && col >= 0) {
-
             if (matrix[row][col] == key) {
-                System.out.print("Key found at (" + row + "," + col + ")");
+                System.out.println("Key found at (" + row + "," + col + ")");
                 return true;
-            }
-
-            else if (key < matrix[row][col]) {
+            } else if (key < matrix[row][col]) {
+                // Move left if the key is smaller
                 col--;
-            }
-
-            else {
+            } else {
+                // Move down if the key is larger
                 row++;
             }
         }
 
-        System.out.print("Key is not present in MATRIX");
+        // If we exit the loop, the key is not present
+        System.out.println("Key is not present in MATRIX");
         return false;
-
     }
 
-    // As a homework
-    // here we are starting serch from bottom left corner
+    // Staircase search starting from the bottom-left corner
     public static boolean staircase_Search_bottomLeft(int matrix[][], int key) {
-
+        // Start at the bottom-left corner
         int row = matrix.length - 1, col = 0;
 
+        // Loop until we either find the key or move out of bounds
         while (row >= 0 && col < matrix[0].length) {
-
-            if (key == matrix[row][col]) {
-                System.out.print("Key found at (" + row + "," + col + ")");
+            if (matrix[row][col] == key) {
+                System.out.println("Key found at (" + row + "," + col + ")");
                 return true;
-            }
-
-            else if(key < matrix[row][col]){
+            } else if (key < matrix[row][col]) {
+                // Move up if the key is smaller
                 row--;
-            }
-
-            else{
+            } else {
+                // Move right if the key is larger
                 col++;
             }
         }
 
-        System.out.print("Key not found !");
+        // If we exit the loop, the key is not present
+        System.out.println("Key not found!");
         return false;
     }
 
     public static void main(String args[]) {
-
+        // Sample sorted matrix
         int matrix[][] = {
                 { 10, 20, 30, 40 },
                 { 15, 25, 35, 45 },
@@ -60,13 +57,13 @@ public class staircase_search_sortted_arr {
                 { 32, 33, 39, 50 }
         };
 
+        // Key to search
         int key = 40;
+
+        // Call the bottom-left staircase search
         staircase_Search_bottomLeft(matrix, key);
 
-        
-
-        
-
+        // Optional: Call the upper-right staircase search
+        // staircase_Search_upperRight(matrix, key);
     }
-
 }
