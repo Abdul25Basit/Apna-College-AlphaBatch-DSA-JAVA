d4a302839d5b3d78313249f6a7b5031923134d0a
Abdul25Basit <136110055+Abdul25Basit@users.noreply.github.com>
2024-12-14 21:37:53 +0530

    Update bubble_sort.java

--- Basic sorting algorithm/bubble_sort.java
+++ Basic sorting algorithm/bubble_sort.java
@@ -1,39 +1,31 @@
-public class bubble_sort {
-    public static void bubbleSort(int array []){
-
-         for(int turn = 0; turn < array.length-1; turn++){
-            for (int j = 0; j < array.length-1-turn; j++) {
-
-                
-                if(array[j] > array[j+1]){
-                    //swap
+public class BubbleSort {
+    public static void bubbleSort(int array[]) {
+        for (int turn = 0; turn < array.length - 1; turn++) {
+            boolean swapped = false;  // To track if any swapping happens
+            
+            for (int j = 0; j < array.length - 1 - turn; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Swap
                     int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
                 }
-                
+            }
+
+            // If no elements were swapped, the array is already sorted
+            if (!swapped) {
+                break;
             }
         }
-
     }
 
-    public static void main(String arg []){
-         
-        int array [] = {5,4,1,3,2};
+    public static void main(String arg[]) {
+        int array[] = {5, 4, 1, 3, 2};
         bubbleSort(array);
 
         for (int i = 0; i < array.length; i++) {
-        System.out.print(array[i]+" ");            
+            System.out.print(array[i] + " ");
         }
-
-    }   
-
-    
+    }
 }
-
-
-
-
-
-
-
